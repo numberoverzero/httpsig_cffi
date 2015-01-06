2e753ab525af7ad7a3fb78160dc4f1b38ceac5de
HawkOwl <hawkowl@atleastfornow.net>
2015-01-06 20:10:40 +0800

    tabs

--- httpsig/sign.py
+++ httpsig/sign.py
@@ -15,20 +15,20 @@ class Signer(object):
     """
     When using an RSA algo, the secret is a PEM-encoded private key.
     When using an HMAC algo, the secret is the HMAC signing secret.
-    
+
     Password-protected keyfiles are not supported.
     """
     def __init__(self, secret, algorithm=None):
         if algorithm is None:
             algorithm = DEFAULT_SIGN_ALGORITHM
-        
+
         assert algorithm in ALGORITHMS, "Unknown algorithm"
         if isinstance(secret, six.string_types): secret = secret.encode("ascii")
-        
+
         self._rsa = None
         self._hash = None
         self.sign_algorithm, self.hash_algorithm = algorithm.split('-')
-        
+
         if self.sign_algorithm == 'rsa':
 
             try:
@@ -36,11 +36,11 @@ class Signer(object):
                 self._rsa = serialization.load_pem_traditional_openssl_private_key(secret, None, backend=default_backend())
             except ValueError, e:
                 raise HttpSigException("Invalid key.")
-                         
-            
-            
+
+
+
         elif self.sign_algorithm == 'hmac':
-            
+
             self._hash = hmac.HMAC(secret,
                                    HASHES[self.hash_algorithm](),
                                    backend=default_backend())
@@ -86,7 +86,7 @@ class HeaderSigner(Signer):
     def __init__(self, key_id, secret, algorithm=None, headers=None):
         if algorithm is None:
             algorithm = DEFAULT_SIGN_ALGORITHM
-        
+
         super(HeaderSigner, self).__init__(secret=secret, algorithm=algorithm)
         self.headers = headers or ['date']
         self.signature_template = build_signature_template(key_id, algorithm, headers)
@@ -103,9 +103,8 @@ class HeaderSigner(Signer):
         headers = CaseInsensitiveDict(headers)
         required_headers = self.headers or ['date']
         signable = generate_message(required_headers, headers, host, method, path)
-        
+
         signature = self._sign(signable)
         headers['authorization'] = self.signature_template % signature
-        
-        return headers
 
+        return headers
